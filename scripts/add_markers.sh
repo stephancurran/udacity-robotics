1a488ed1f8cbe64377963f4a2c72ff3b72acd2bc
Stephan Curran <stephan.curran@gmail.com>
2020-06-22 22:34:06 +0000

    Added demo mode to add_markers
    Rubric requires rviz demo of showing marker, pausing, deleting marker, pausing, readding marker - all independent of robot

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -6,5 +6,5 @@ xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm -e "roslaunch add_markers add_markers.launch" &
+xterm -e "roslaunch add_markers add_markers_test.launch" &
 sleep 5
